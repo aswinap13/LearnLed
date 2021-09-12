99268e0c8e1bbc4ab83d6ef27bc0406c6f7c4e52
Aswin <aswinapxyz@cet.ac.in>
2021-09-12 12:29:22 +0530

    Modified Project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-$(document).ready(function(){
-    AOS.init();
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-})
+$( document ).ready(function() {
+        $('.navbar-collapse a').click(function(){
+            $(".navbar-collapse").collapse('hide');
+        }); 
+        AOS.init();
+        var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+        var rootElement = document.documentElement
+
+        function scrollToTop() {
+        // Scroll to top logic
+        rootElement.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+        }
+        scrollToTopBtn.addEventListener("click", scrollToTop)
+});
